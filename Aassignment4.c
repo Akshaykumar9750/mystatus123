fc8112b455a1277e76eb6661ab95917a02d9823d
Akshaykumar9750 <107535118+Akshaykumar9750@users.noreply.github.com>
2022-07-31 15:42:52 +0530

    Add files via upload

---
+++ Aassignment4.c
@@ -0,0 +1,160 @@
+/*
+//Q.1
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        printf("MySirG \n");
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.2
+int main()
+{
+    int i=1;
+    while(i<11)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.3
+int main()
+{
+    int i=10;
+    while(i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+    return 0;
+}
+*/
+/*
+//Q.4
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i*2-1);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.5
+int main()
+{
+    int i=10;
+    while(i>=1)
+    {
+        printf("%d\n",i*2-1);
+        i--;
+    }
+    return 0;
+}
+*/
+/*
+//Q.6
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i*2);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.7
+int main()
+{
+    int i=10;
+    while(i>=1)
+    {
+        printf("%d\n",i*2);
+        i--;
+    }
+    return 0;
+}
+*/
+/*
+//Q.8
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.9
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    return 0;
+}
+*/
+
+//Q.10
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("5 * %d = %d\n",i,i*5);
+        i++;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
