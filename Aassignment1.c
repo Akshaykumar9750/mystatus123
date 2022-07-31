c92b65cb089f2dc542b8fb1ad572c189867851ba
Akshaykumar9750 <107535118+Akshaykumar9750@users.noreply.github.com>
2022-07-31 15:29:15 +0530

    Add files via upload

---
+++ Aassignment1.c
@@ -0,0 +1,121 @@
+/*
+// Q.1
+main()
+{
+    printf("Hello Student");
+
+}
+*/
+/*
+// Q.2
+main()
+{
+    printf("Hello\nStudent");
+
+}
+*/
+/*
+// Q.3
+main()
+{
+    printf("\"MySirG\"");
+}
+*/
+/*
+// Q.4
+main()
+{
+    int r;
+    float a;
+    printf("Enter  radious of circle");
+    scanf("%d",&r);
+    a=3.14*r*r;
+    printf("\"Area of circle is %f having the radious %d\"",a,r);
+}
+*/
+/*
+// Q.5
+main()
+{
+int x;
+x=printf("String");
+printf("\n Length of String is :%d",x);
+}
+*/
+/*
+// Q.6
+main()
+{
+    printf("\"Hello,Amit kumar\"");
+}
+*/
+/*
+// Q.7
+main()
+{
+    char a='%',b='d';
+    printf("%c%c\n",a,b);
+    //2nd method
+    printf("%%d");
+}
+*/
+/*
+//Q.8
+main()
+{
+    printf("\\n");
+    getch();
+}
+*/
+/*
+// Q.9
+main()
+{
+    printf("\\\\");
+}
+*/
+/*
+// Q.10
+main()
+{
+    int DD,YYYY,MM;
+    printf("Enter date in DD/MM/YYYY format\n");
+    scanf("%d/%d/%d",&DD,&MM,&YYYY);
+    printf("Day- %d,Month- %d,Year- %d",DD,MM,YYYY);
+}
+*/
+/*
+//Q.11
+main()
+{
+    int HH,MM;
+    printf("Enter time in \"HH:MM\" format");
+    scanf("%d:%d",&HH,&MM);
+    printf("\"%d Hour and %d Minut\"",HH,MM);
+}
+*/
+/*
+//Q.12
+int main()
+{
+    int x=printf("ineuron");
+    printf("\n%d",x);
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
