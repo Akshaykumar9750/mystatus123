7ae381f53e2a6a417aefcf3c29a192a889b54bc5
Akshaykumar9750 <107535118+Akshaykumar9750@users.noreply.github.com>
2022-07-31 15:43:56 +0530

    Add files via upload

---
+++ Aassignment5.c
@@ -0,0 +1,169 @@
+/*
+//Q.1
+int main()
+{
+    int N,i=1;
+    printf("Enter a number");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.2
+int main()
+{
+    int N,i=1;
+    printf("Enter a number :");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.3
+int main()
+{
+    int N;
+    printf("Enter a number :");
+    scanf("%d",&N);
+    while(N>=1)
+    {
+        printf("%d\n",N);
+        N--;
+    }
+    return 0;
+}
+*/
+/*
+//Q.4
+int main()
+{
+    int N,i=1;
+    printf("Enter a number :");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i*2-1);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.5
+int main()00
+{
+    int N;
+    printf("Enter a number :");
+    scanf("%d",&N);
+    while(N>=1)
+    {
+        printf("%d\n",N*2-1);
+        N--;
+    }
+    return 0;
+}
+*/
+/*
+//Q.6
+int main()
+{
+    int N,i=1;
+    printf("Enter a number :");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i*2);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.7
+int main()
+{
+    int N;
+    printf("Enter a number :");
+    scanf("%d",&N);
+    while(N>=1)
+    {
+        printf("%d\n",N*2);
+        N--;
+    }
+    return 0;
+}
+*/
+/*
+//Q.8
+int main()
+{
+    int N,i=1;
+    printf("Enter a number");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }
+    return 0;
+}
+*/
+/*
+//Q.9
+int main()
+{
+    int N,i=1;
+    printf("Enter a number");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    return 0;
+}
+*/
+
+//Q.10
+int main()
+{
+    int N,j,i=1;
+    printf("Enter table number :");
+    scanf("%d",&N);
+    while(i<=10)
+    {
+        printf("%d * %d = %d\n",N,i,N*i);
+        i++;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
